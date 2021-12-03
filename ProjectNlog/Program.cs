35028122e72d29542d2920575ecade403d736c03
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-03 21:03:41 +0530

    [Rutwik]: Added Logger File

---
+++ ProjectNlog/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers add = new AddNumbers();
+            add.Sum(6, 7);
+        }
+    }
+}
